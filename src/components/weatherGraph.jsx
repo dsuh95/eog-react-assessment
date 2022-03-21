e127bafe7a01f6e3072dda40eee198c11b8f4c96
dsuh95 <d_suh95@yahoo.com>
2022-03-21 10:31:28 -0700

    moved comments

--- src/components/weatherGraph.jsx
+++ src/components/weatherGraph.jsx
@@ -4,14 +4,14 @@ import {
 } from 'recharts';
 import { useSelector } from 'react-redux';
 
+//  I was unable to see any schemas or any information in the API Documentation
+//    so I simply implemented a graphing feature for temperature.
+//  A button to add entries was added to the header in the Weather.tsx file
+//    but what I'm thinking in terms of the future is that the button will
+//    be removed and the readings will be automatically fetched every 1.3 minutes.
+
 const WeatherGraph = () => {
   const weather = useSelector((state) => state.weather.value);
-
-  //  I was unable to see any schemas or any information in the API Documentation
-  //    so I simply implemented a graphing feature for temperature.
-  //  A button to add entries was added to the header in the Weather.tsx file
-  //    but what I'm thinking in terms of the future is that the button will
-  //    be removed and the readings will be automatically fetched every 1.3 minutes.
 
   return (
     <div id='weatherGraph' margin={{ top: 50 }}>
@@ -27,8 +27,8 @@ const WeatherGraph = () => {
       <ResponsiveContainer width='100%' aspect={3}>
         <LineChart width={50} height={50} margin={{ top: 50 }} data={weather}>
           <Legend verticalAlign="bottom" height={36} />
-          <Line name='Temperature In Celsius' type="monotone" dataKey="temperatureinCelsius" stroke="#8884d8" />;
-          <CartesianGrid stroke="#ccc" />
+          <Line name='Temperature In Celsius' type="monotone" dataKey="temperatureinCelsius" stroke="darkblue" />;
+          <CartesianGrid stroke="lightblue" />
           <XAxis dataKey="name" />
           <YAxis />
         </LineChart>
