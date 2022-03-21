c1a43621cf44b1e28ba28638d1ae361045a80e39
dsuh95 <d_suh95@yahoo.com>
2022-03-21 10:04:40 -0700

    Added legend

--- src/components/weatherGraph.jsx
+++ src/components/weatherGraph.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import {
-  LineChart, Line, ResponsiveContainer, CartesianGrid, XAxis, YAxis,
+  LineChart, Line, ResponsiveContainer, CartesianGrid, XAxis, YAxis, Legend,
 } from 'recharts';
 import { useSelector } from 'react-redux';
 
 const WeatherGraph = () => {
   const weather = useSelector((state) => state.weather.value);
-  // const weatherData = weather.map((entry) => (entry.temperatureinCelsius));
 
   return (
     <ResponsiveContainer width='100%' aspect={3}>
-      <LineChart width={50} height={50} margin={5} data={weather}>
-        <Line type="monotone" dataKey="temperatureinCelsius" stroke="#8884d8" />;
+      <LineChart width={50} height={50} margin={{ top: 50 }} data={weather}>
+        <Legend verticalAlign="bottom" height={36} />
+        <Line name='Temperature In Celsius' type="monotone" dataKey="temperatureinCelsius" stroke="#8884d8" />;
         <CartesianGrid stroke="#ccc" />
         <XAxis dataKey="name" />
         <YAxis />
