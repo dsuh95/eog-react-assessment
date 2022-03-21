b4ae059c1af586e74c9c44680f322c9522d7beab
dsuh95 <d_suh95@yahoo.com>
2022-03-21 10:11:51 -0700

    Added text with most recent reading added to graph

--- src/components/weatherGraph.jsx
+++ src/components/weatherGraph.jsx
@@ -8,15 +8,26 @@ const WeatherGraph = () => {
   const weather = useSelector((state) => state.weather.value);
 
   return (
-    <ResponsiveContainer width='100%' aspect={3}>
-      <LineChart width={50} height={50} margin={{ top: 50 }} data={weather}>
-        <Legend verticalAlign="bottom" height={36} />
-        <Line name='Temperature In Celsius' type="monotone" dataKey="temperatureinCelsius" stroke="#8884d8" />;
-        <CartesianGrid stroke="#ccc" />
-        <XAxis dataKey="name" />
-        <YAxis />
-      </LineChart>
-    </ResponsiveContainer>
+    <div id='weatherGraph' margin={{ top: 50 }}>
+      {
+      weather[0]
+        ? (
+          <h2>
+            Current Temperature in C: {weather[0].temperatureinCelsius}
+          </h2>
+        )
+        : null
+      }
+      <ResponsiveContainer width='100%' aspect={3}>
+        <LineChart width={50} height={50} margin={{ top: 50 }} data={weather}>
+          <Legend verticalAlign="bottom" height={36} />
+          <Line name='Temperature In Celsius' type="monotone" dataKey="temperatureinCelsius" stroke="#8884d8" />;
+          <CartesianGrid stroke="#ccc" />
+          <XAxis dataKey="name" />
+          <YAxis />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
   );
 };
 
